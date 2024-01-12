dc7b10498e222996839f52ce88f5f0585b68bb4c
kimq6 <kimq6@hanyang.ac.kr>
2024-01-12 20:31:59 +0900

    x 움직여서 사인 그래프 같은거 나오는 3번째 그래프 그리기

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 
 # 그래프 설정
 fig = plt.figure(figsize=(12, 8))
-ax0 = fig.add_subplot(121, projection="3d", title="graphene")  # 3D설정
+ax0 = fig.add_subplot(131, projection="3d", title="graphene")  # 3D설정
 ax0.view_init(elev=90, azim=0)  # 보는 각도 설정
-ax1 = fig.add_subplot(122, title="z-potential")  # z-potential 그래프
-ax2 = fig.add_subplot(122 title="x-potential")
+ax1 = fig.add_subplot(132, title="z-potential")  # z-potential 그래프
+ax2 = fig.add_subplot(133, title="x-potential")
 
 
 lattice_constant = 0.142  # 격자 상수
@@ -29,29 +29,33 @@ ax0.scatter(0, 0, 0.1, c="r")  # 팁 원자 점 찍기(빨간색)
 graphene_xyz = list(zip(x_grid, y_grid, [0.0 for x in range(len(x_grid))]))  # 그래핀 원자들의 좌표(xyz)
 
 
-def potential_3d(cor1, cor2, d=0.205, x=0.4073, sigma=2.5):  # cor1, cor2: 좌표 2개, d: DIJ, x: xIJ, sigma: 몇 시그마까지 할지
+def potential_3d(cor1, cor2, d_ij=0.205, x_ij=0.4073, sigma=2.5):  # cor1, cor2: 좌표 2개, d: DIJ, x: xIJ, sigma: 몇 시그마까지 할지
     distance = np.sqrt(math.pow((cor1[0]-cor2[0]), 2) + math.pow((cor1[1]-cor2[1]), 2) + math.pow((cor1[2]-cor2[2]), 2))
-    if distance < sigma * math.pow(2, -1/6) * x:  # 시그마 안의 거리에 있는 원자는 그냥 포텐셜 값 구하기
-        return d * (math.pow((x/distance), 12) - 2 * math.pow((x/distance), 6))
+    if distance < sigma * math.pow(2, -1/6) * x_ij:  # 시그마 안의 거리에 있는 원자는 그냥 포텐셜 값 구하기
+        return d_ij * (math.pow((x_ij/distance), 12) - 2 * math.pow((x_ij/distance), 6))
     else:  # 시그마 값 넘는 원자는 potential 0 으로 취급
         return 0
 
 
+# (0, 0, z)에서 z움직이면서 potential 측정
 z_potential_min = 0  # potential 합이 최소일 때 z값
 potential_sum_min = 100  # 최소인 potential 합
 for z in np.arange(0.3, 0.6, 0.001):  # z의 범위[nm]
     potential_sum = 0  # potential의 합 초기화
     for i in range(len(graphene_xyz)):  # 그래핀 원자 수만큼 반복
-        potential_sum += potential_3d(graphene_xyz[i], (0, 0, z))  # i번째 그래핀 원자와 팁원자(0, 0, z)간의 거리를 함수로 구해서 누적
-    print(potential_sum, z)
+        potential_sum += potential_3d(graphene_xyz[i], (0, 0, z))  # i번째 그래핀 원자와 팁원자(0, 0, z)간의 potential을 함수로 구해서 누적
     ax1.scatter(z, potential_sum)  # 두번째 그래프 점 찍기
     if potential_sum_min > potential_sum:  # 여태까지의 최소값이 구한 값보다 크다면(최솟값을 갱신하면)
         z_potential_min = z  # z값 저장
         potential_sum_min = potential_sum  # potential값 저장
 
+print(potential_sum_min, z_potential_min)
+
+# z_potential_min 에서 x 움직이면서 potential 측정, 그래프 그리기
+for x in np.arange(0, 1, 0.001):
+    potential_sum = 0  # potential의 합 초기화
+    for i in range(len(graphene_xyz)):  # 그래핀 원자 수만큼 반복
+        potential_sum += potential_3d(graphene_xyz[i], (x, 0, z_potential_min))  # i번째 그래핀 원자와 팁원자(x, 0, z_potential_min)간의 potential을 함수로 구해서 누적
+    ax2.scatter(x, potential_sum)  # 세번째 그래프 점 찍기
 
 plt.show()
-
-
-# z_potential_min에서 xy 움직이면서 potential 측정.
-
